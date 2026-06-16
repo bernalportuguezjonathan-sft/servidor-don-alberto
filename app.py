8e1b6cb9033673606b9d93ffdc50e2a9cd59b555
bernalportuguezjonathan-sft <bernalportuguezjonathan@gmail.com>
2026-06-16 08:37:07 -0500

    Asignacion de puerto para render

--- app.py
+++ app.py
@@ -35,4 +35,6 @@ def peritajes():
     return jsonify(datos)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
